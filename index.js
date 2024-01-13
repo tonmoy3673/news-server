1d29713e95c8f6b295bd880752b7adb46c0e3c62
tonmoy <tonmoy3673@gmail.com>
2024-01-13 12:59:06 +0600

    set port

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
  const express=require('express');
  const cors=require('cors');
  const categories=require('./Categories.json');
+ const app=express();
+ const port=5000;
+ app.use(cors());
+ app.get('/',(req,res)=>{
+    res.send('News Server is Running')
+ });
+
+ app.listen(port,()=>{
+    console.log(`News Server is running on port,{port}`)
+ });
  
